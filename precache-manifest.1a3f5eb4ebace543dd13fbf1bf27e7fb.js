7bd20cdb52d6b1a0c3a119bd1842347af94f5987
Peter Wang <pw2@andrew.cmu.edu>
2023-01-25 23:48:43 -0500

    Updates

---
+++ precache-manifest.1a3f5eb4ebace543dd13fbf1bf27e7fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "822726da484fa16e783d",
+    "url": "/vicweb/static/css/main.524f2aa5.chunk.css"
+  },
+  {
+    "revision": "822726da484fa16e783d",
+    "url": "/vicweb/static/js/main.822726da.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/vicweb/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/vicweb/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b5148b6238bfcd8df9f8",
+    "url": "/vicweb/static/js/runtime~main.b5148b62.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/vicweb/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "092aaba47ed6dd2f4d37b8a801efdd59",
+    "url": "/vicweb/index.html"
+  }
+];
